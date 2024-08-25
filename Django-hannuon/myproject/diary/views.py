08cbb920e43cbef2a98de27bfddb4ef725a2532c
kawafuchi <k06en23@gmail.com>
2024-08-25 18:23:21 +0900

    ログインを強化

--- Django-hannuon/myproject/diary/views.py
+++ Django-hannuon/myproject/diary/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import View
 from .forms import PageForm
 from .models import Page
@@ -6,7 +7,7 @@ from datetime import datetime
 from zoneinfo import ZoneInfo
 
 
-class IndexView(View):
+class IndexView(LoginRequiredMixin, View):
     def get(self, request):
         datetime_now = datetime.now(
             ZoneInfo("Asia/Tokyo")
@@ -15,7 +16,7 @@ class IndexView(View):
             request, "diary/index.html", {"datetime_now": datetime_now})
     
 
-class PageCreateView(View):
+class PageCreateView(LoginRequiredMixin, View):
     def get(self, request):
         form = PageForm()
         return render(request, "diary/page_form.html", {"form": form})
@@ -27,18 +28,18 @@ class PageCreateView(View):
             return redirect("diary:index")
         return render(request, "diary/page_form.html", {"form": form})
 
-class PageListView(View):
+class PageListView(LoginRequiredMixin, View):
     def get(self, request):
         # ("page_date")昇順　("-page_date")降順
         page_list = Page.objects.order_by("-page_date")
         return render(request, "diary/page_list.html", {"page_list": page_list})
 
-class PageDetailView(View):
+class PageDetailView(LoginRequiredMixin, View):
     def get(self, request, id):
         page = get_object_or_404(Page, id=id)
         return render(request, "diary/page_detail.html", {"page": page})
 
-class PageUpdateView(View):
+class PageUpdateView(LoginRequiredMixin, View):
     def get(self, request, id):
         page = get_object_or_404(Page, id=id)
         form = PageForm(instance=page)
@@ -52,7 +53,7 @@ class PageUpdateView(View):
             return redirect("diary/page_detail", id=id)
         return render(request, "diary/page_form.html", {"form": form})
     
-class PageDeleteView(View):
+class PageDeleteView(LoginRequiredMixin, View):
     def get(self, request, id):
         page = get_object_or_404(Page, id=id)
         return render(request, "diary/page_confirm_delete.html", {"page": page})
